fa04d4b7f4ce45d52cc98dde89efcba0170f1e31
Sabas Vega <sabascarloseduardo@gmail.com>
2019-03-05 15:05:32 -0400

    Fix in migration pre inspection pivot table

--- Database/Migrations/2019_02_16_125000_create_icda_pre_inspections_pivots_table.php
+++ Database/Migrations/2019_02_16_125000_create_icda_pre_inspections_pivots_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateIcdaAxesTable extends Migration
+class CreateIcdaPreInspectionsPivotsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,13 @@ class CreateIcdaAxesTable extends Migration
      */
     public function up()
     {
-        Schema::create('icda__axes', function (Blueprint $table) {
+        Schema::create('icda__pre_inspections_pivots', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('inspections_id')->unsigned();
-            $table->foreign('inspections_id')->references('id')->on('icda__inspections');
-            $table->text('values');
+            $table->integer('icda__pre_inspections_id')->unsigned();
+            $table->foreign('icda__pre_inspections_id')->references('id')->on('icda__pre_inspections');
+            $table->integer('icda__inspections_id')->unsigned();
+            $table->foreign('icda__inspections_id')->references('id')->on('icda__inspections');
+            $table->string('value');//Value
             $table->timestamps();
         });
     }
@@ -29,6 +31,6 @@ class CreateIcdaAxesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('icda__axes');
+        Schema::dropIfExists('icda__pre_inspections_pivots');
     }
 }
